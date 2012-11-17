7b30ec06f1bafc28d23073f307f05df63b1ba2e6
joshlisko <jpl4ke@virginia.edu>
2012-11-17 18:02:27 -0500

    adding GUI framework

--- Twitiment/src/edu/cs4240/GUI/MainJFrame.java
+++ Twitiment/src/edu/cs4240/GUI/MainJFrame.java
@@ -1,9 +1,43 @@
 package edu.cs4240.GUI;
+import com.cloudgarden.layout.AnchorConstraint;
+import com.cloudgarden.layout.AnchorLayout;
+import java.awt.BorderLayout;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JButton;
+import javax.swing.JTextField;
 
 import javax.swing.WindowConstants;
 import javax.swing.SwingUtilities;
 
+
+/**
+* This code was edited or generated using CloudGarden's Jigloo
+* SWT/Swing GUI Builder, which is free for non-commercial
+* use. If Jigloo is being used commercially (ie, by a corporation,
+* company or business for any purpose whatever) then you
+* should purchase a license for each developer using Jigloo.
+* Please visit www.cloudgarden.com for details.
+* Use of Jigloo implies acceptance of these licensing terms.
+* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED FOR
+* THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED
+* LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
+*/
 public class MainJFrame extends javax.swing.JFrame {
+	private JTextField QueryJTextField;
+	private JButton SearchJButton;
+
+	{
+		//Set Look & Feel
+		try {
+			javax.swing.UIManager.setLookAndFeel(javax.swing.UIManager.getSystemLookAndFeelClassName());
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 
 	/**
 	* Auto-generated main method to display this JFrame
@@ -16,13 +50,53 @@ public class MainJFrame extends javax.swing.JFrame {
 	
 	private void initGUI() {
 		try {
+			AnchorLayout thisLayout = new AnchorLayout();
+			getContentPane().setLayout(thisLayout);
 			setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+			{
+				SearchJButton = new JButton();
+				AnchorLayout SearchJButtonLayout = new AnchorLayout();
+				SearchJButton.setLayout(SearchJButtonLayout);
+				getContentPane().add(SearchJButton, new AnchorConstraint(570, 986, 674, 701, AnchorConstraint.ANCHOR_REL, AnchorConstraint.ANCHOR_REL, AnchorConstraint.ANCHOR_REL, AnchorConstraint.ANCHOR_REL));
+				SearchJButton.setText("Search");
+				SearchJButton.setPreferredSize(new java.awt.Dimension(114, 29));
+				SearchJButton.addMouseListener(new MouseAdapter() {
+					public void mouseClicked(MouseEvent evt) {
+						SearchJButtonMouseClicked(evt);
+					}
+				});
+			}
+			{
+				QueryJTextField = new JTextField();
+				getContentPane().add(QueryJTextField, new AnchorConstraint(422, 1001, 526, 6, AnchorConstraint.ANCHOR_REL, AnchorConstraint.ANCHOR_REL, AnchorConstraint.ANCHOR_REL, AnchorConstraint.ANCHOR_ABS));
+				QueryJTextField.setPreferredSize(new java.awt.Dimension(394, 29));
+				QueryJTextField.addKeyListener(new KeyAdapter() {
+					public void keyPressed(KeyEvent evt) {
+						QueryJTextFieldKeyPressed(evt);
+					}
+				});
+			}
 			pack();
 			setSize(400, 300);
 		} catch (Exception e) {
 		    //add your error handling code here
 			e.printStackTrace();
 		}
+	}
+	
+	private void SearchJButtonMouseClicked(MouseEvent evt) {
+		System.out.println("SearchJButton.mouseClicked, event="+evt);
+		Search();
+	}
+	
+	private void QueryJTextFieldKeyPressed(KeyEvent evt) {
+		System.out.println("QueryJTextField.keyPressed, event="+evt);
+		Search();
+	}
+	
+	private void Search(){
+		//do something with Query QueryJTextField.getText()
+	
 	}
 
 }
---
+++ Twitiment/src/edu/cs4240/GUI/ResultsJFrame.java
@@ -0,0 +1,48 @@
+package edu.cs4240.GUI;
+import java.awt.BorderLayout;
+import javax.swing.JTextPane;
+
+import javax.swing.WindowConstants;
+import javax.swing.SwingUtilities;
+
+
+/**
+* This code was edited or generated using CloudGarden's Jigloo
+* SWT/Swing GUI Builder, which is free for non-commercial
+* use. If Jigloo is being used commercially (ie, by a corporation,
+* company or business for any purpose whatever) then you
+* should purchase a license for each developer using Jigloo.
+* Please visit www.cloudgarden.com for details.
+* Use of Jigloo implies acceptance of these licensing terms.
+* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED FOR
+* THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED
+* LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
+*/
+public class ResultsJFrame extends javax.swing.JFrame {
+	private JTextPane ResultsJTextPane;
+
+	/**
+	* Auto-generated main method to display this JFrame
+	*/
+		
+	public ResultsJFrame() {
+		super();
+		initGUI();
+	}
+	
+	private void initGUI() {
+		try {
+			setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+			{
+				ResultsJTextPane = new JTextPane();
+				getContentPane().add(ResultsJTextPane, BorderLayout.CENTER);
+			}
+			pack();
+			setSize(400, 300);
+		} catch (Exception e) {
+		    //add your error handling code here
+			e.printStackTrace();
+		}
+	}
+
+}
